cbdfc1659122c007609ac36641133ad97b6edef7
rightpunchChen <rightpunch1123@gmail.com>
2025-01-24 19:34:17 +0800

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,298 @@
+import vtk
+import imageio
+from vtkmodules.util import numpy_support as vtkutil
+from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QFileDialog, QMessageBox
+
+from ui_window import Ui_MainWindow
+from utiles.vtk_tools import *
+from utiles.configs import *
+
+class MainWindowController(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.init()
+        self.init_actor()
+        
+        self.brain_image_vtk = None
+        self.label_image = None
+        self.label_image_vtk = None
+        self.pred_image_vtk = None
+
+    def init_actor(self):
+        self.brain_actor = None
+        self.label_actor = None
+        self.tp_actor = None
+        self.fp_actor = None
+        self.fn_actor = None
+
+    def init(self):
+        self.ui.BF_btn.clicked.connect(self.open_brain_file) 
+        self.ui.LF_btn.clicked.connect(self.open_label_file)
+        self.ui.PF_btn.clicked.connect(self.open_prediction_file)
+
+        self.ui.BO_spinBox.valueChanged.connect(self.set_brain_opacity)
+        self.ui.LO_spinBox.valueChanged.connect(self.set_label_opacity)
+        self.ui.PO_spinBox.valueChanged.connect(self.set_pred_opacity)
+        self.ui.render_pushButton.clicked.connect(self.render_brain)
+        self.ui.save_pushButton.clicked.connect(self.save_mp4)
+
+        self.ui.BF_lineEdit.returnPressed.connect(self.update_render_button)
+        self.ui.LF_lineEdit.returnPressed.connect(self.update_label_button)
+        self.ui.PF_lineEdit.returnPressed.connect(self.update_pred_button)
+        # self.ui.PF_lineEdit.textChanged.connect(self.update_pred_button)
+        # self.ui.LF_lineEdit.textChanged.connect(self.update_label_button)
+        # self.ui.BF_lineEdit.textChanged.connect(self.update_render_button)
+
+        self.layout = QVBoxLayout(self.ui.render)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.vtk_widget = QVTKRenderWindowInteractor(self.ui.render)
+        self.layout.addWidget(self.vtk_widget)  # Add the VTK widget to the layout
+
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(BACKGROUND_COLORS)
+        self.render_window = self.vtk_widget.GetRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+        self.interactor = self.render_window.GetInteractor()
+        self.interactor.Initialize()
+
+    def show_error_message(self, message):
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Critical)
+        msg_box.setText(message)
+        msg_box.setWindowTitle("Error")
+        msg_box.exec()
+
+    # def load_image_(self, file_path):
+    #     try:
+    #         return load_image(file_path)
+    #     except Exception as e:
+    #         self.show_error_message(f"Failed to load file: {file_path}\nError: {str(e)}")
+    #         return None
+    #     # if not os.path.exists(file_path):
+    #     #     self.show_error_message(f"File does not exist: {file_path}")
+
+    def open_brain_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.Option.DontUseNativeDialog
+        file_path, _ = QFileDialog.getOpenFileName(self,"Select NII files", "", "NII Files (*.nii *.nii.gz)", options=options)
+        self.ui.BF_lineEdit.setText(file_path)
+        self.update_render_button()
+    
+    def open_label_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.Option.DontUseNativeDialog
+        file_path, _ = QFileDialog.getOpenFileName(self,"Select NII files", "", "NII Files (*.nii *.nii.gz)", options=options)
+        self.ui.LF_lineEdit.setText(file_path)
+        self.update_label_button()
+        
+    def open_prediction_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.Option.DontUseNativeDialog
+        file_path, _ = QFileDialog.getOpenFileName(self,"Select NII files", "", "NII Files (*.nii *.nii.gz)", options=options)
+        self.ui.PF_lineEdit.setText(file_path)
+        self.update_label_button()
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        for url in event.mimeData().urls():
+            file_path = url.toLocalFile()
+            if file_path.endswith(('.nii', '.nii.gz')):
+                if self.ui.BF_lineEdit.hasFocus():
+                    self.ui.BF_lineEdit.setText(file_path)
+                    self.update_render_button()
+                elif self.ui.LF_lineEdit.hasFocus():
+                    self.ui.LF_lineEdit.setText(file_path)
+                    self.update_label_button()
+                elif self.ui.PF_lineEdit.hasFocus():
+                    self.ui.PF_lineEdit.setText(file_path)
+                    self.update_pred_button()
+                break
+
+    def update_render_button(self):
+        file_path = self.ui.BF_lineEdit.text()
+        if check_files(file_path):
+            self.ui.LF_btn.setEnabled(True)
+            self.ui.LF_lineEdit.setEnabled(True)
+            self.ui.render_pushButton.setEnabled(True)
+            return
+        elif not check_files(file_path):
+            self.show_error_message(f"File does not exist: {file_path}")
+
+        self.ui.LF_btn.setEnabled(False)
+        self.ui.LF_lineEdit.setEnabled(False)
+        self.ui.render_pushButton.setEnabled(False)
+        self.ui.BO_spinBox.setEnabled(False)
+        self.ui.save_pushButton.setEnabled(False)
+
+    def update_label_button(self):
+        file_path = self.ui.LF_lineEdit.text()
+        if check_files(file_path):
+            self.label_image_vtk = load_image(file_path)
+            self.label_image = vtk_img_to_numpy(self.label_image_vtk)
+            max_label_value = self.label_image.max()
+            for i in range(1, min(int(max_label_value) + 1, LABEL_NUM + 1)):
+                getattr(self.ui, f'radioButton_{i}').setEnabled(True)
+            self.ui.PF_btn.setEnabled(True)
+            self.ui.PF_lineEdit.setEnabled(True)
+            return
+        elif not check_files(file_path):
+            self.show_error_message(f"File does not exist: {file_path}")
+
+        for i in range(1, 6):
+            getattr(self.ui, f'radioButton_{i}').setEnabled(False)
+        self.ui.PF_btn.setEnabled(False)
+        self.ui.PF_lineEdit.setEnabled(False)
+    
+    def update_pred_button(self):
+        file_path = self.ui.PF_lineEdit.text()
+        if check_files(file_path):
+            self.pred_image_vtk = load_image(file_path)
+            for i in ['tp', 'fp', 'fn']:
+                getattr(self.ui, f'radioButton_{i}').setEnabled(True)
+            return
+        elif not check_files(file_path):
+            self.show_error_message(f"File does not exist: {file_path}")
+
+        for i in ['tp', 'fp', 'fn']:
+            getattr(self.ui, f'radioButton_{i}').setEnabled(False)
+
+    def updata_LO_spinBox(self):
+        for i in range(1, LABEL_NUM + 1):
+            radio_button = getattr(self.ui, f'radioButton_{i}')
+            if radio_button.isChecked():
+                self.ui.LO_spinBox.setEnabled(True)
+                return
+        self.ui.LO_spinBox.setEnabled(False)
+        return
+        
+    def updata_PO_spinBox(self):
+        for i in ['tp', 'fp', 'fn']:
+            radio_button = getattr(self.ui, f'radioButton_{i}')
+            if radio_button.isChecked():
+                self.ui.PO_spinBox.setEnabled(True)
+                return
+        self.ui.PO_spinBox.setEnabled(False)
+        return
+
+    def render_brain(self):
+        self.init_actor()
+        self.renderer.RemoveAllViewProps()
+        self.ui.BO_spinBox.setEnabled(True)
+        self.ui.save_pushButton.setEnabled(True)
+
+        brain_file = self.ui.BF_lineEdit.text()
+        self.brain_image_vtk = load_image(brain_file)
+        self.brain_actor = setup_actor(
+            self.brain_image_vtk,
+            self.ui.BO_spinBox.value() / 200,
+            BRAIN_COLORS
+            )
+        self.renderer.AddActor(self.brain_actor)
+
+        if self.label_image is not None:
+            self.updata_LO_spinBox()
+            selected_label = []
+            for i in range(1, LABEL_NUM + 1):
+                radio_button = getattr(self.ui, f'radioButton_{i}')
+                if radio_button.isChecked():
+                    selected_label.append(i)
+            if selected_label:
+                self.label_actor = [None] * (max(selected_label) + 1)
+                for l in range(len(selected_label)):
+                    if(check_label(self.label_image, selected_label[l])): continue
+                    self.label_actor[selected_label[l]] = setup_actor(
+                        self.label_image_vtk,
+                        self.ui.LO_spinBox.value() / 100,
+                        MASK_COLORS[selected_label[l] - 1],
+                        selected_label[l]
+                        )
+                    self.renderer.AddActor(self.label_actor[selected_label[l]])
+        
+        if self.pred_image_vtk is not None:
+            self.updata_PO_spinBox()
+            if self.ui.radioButton_tp.isChecked():
+                tp = AND(self.label_image_vtk, self.pred_image_vtk)
+                if tp is not None:
+                    self.tp_actor = setup_actor(
+                        tp,
+                        self.ui.PO_spinBox.value() / 100,
+                        TP_COLOR
+                        )
+                    self.renderer.AddActor(self.tp_actor)
+                    self.ui.PO_spinBox.setEnabled(True)
+            if self.ui.radioButton_fp.isChecked():
+                fp = false_positive(self.label_image_vtk, self.pred_image_vtk)
+                if fp is not None:
+                    self.fp_actor = setup_actor(
+                        fp,
+                        self.ui.PO_spinBox.value()/100,
+                        FP_COLOR
+                        )
+                    self.renderer.AddActor(self.fp_actor)
+                    self.ui.PO_spinBox.setEnabled(True)
+            if self.ui.radioButton_fn.isChecked():
+                fn = false_negative(self.label_image_vtk, self.pred_image_vtk)
+                if fn is not None:
+                    self.fn_actor = setup_actor(
+                        fn,
+                        self.ui.PO_spinBox.value()/100,
+                        FN_COLOR
+                        )
+                    self.renderer.AddActor(self.fn_actor)
+                    self.ui.PO_spinBox.setEnabled(True)
+                        
+        set_camera(self.renderer)
+        self.render_window.Render()
+    
+    def set_brain_opacity(self):
+        opacity = self.ui.BO_spinBox.value()
+        self.brain_actor.GetProperty().SetOpacity(opacity / 200)
+        self.render_window.Render()
+    
+    def set_label_opacity(self):
+        opacity = self.ui.LO_spinBox.value()
+        for i in range(len(self.label_actor)):
+            if self.label_actor[i] is not None:
+                self.label_actor[i].GetProperty().SetOpacity(opacity / 100)
+        self.render_window.Render()
+
+    def set_pred_opacity(self):
+        opacity = self.ui.PO_spinBox.value()
+        if self.tp_actor is not None:
+            self.tp_actor.GetProperty().SetOpacity(opacity / 100)
+        if self.fp_actor is not None:
+            self.fp_actor.GetProperty().SetOpacity(opacity / 100)
+        if self.fn_actor is not None:
+            self.fn_actor.GetProperty().SetOpacity(opacity / 100)
+        self.render_window.Render()
+
+    def save_mp4(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.Option.DontUseNativeDialog
+        file_path, _ = QFileDialog.getSaveFileName(self, "Save MP4 file", "", "MP4 Files (*.mp4)", options=options)
+        if file_path:
+            window_to_image_filter = vtk.vtkWindowToImageFilter()
+            window_to_image_filter.SetInput(self.render_window)
+            window_to_image_filter.SetInputBufferTypeToRGB()
+            window_to_image_filter.ReadFrontBufferOff()
+
+            frames = []
+
+            for _ in range(360):
+                self.renderer.GetActiveCamera().Azimuth(1)
+                self.render_window.Render()
+                window_to_image_filter.Modified()
+                window_to_image_filter.Update()
+                image_data = window_to_image_filter.GetOutput()
+                width, height, _ = image_data.GetDimensions()
+                frame = vtkutil.vtk_to_numpy(image_data.GetPointData().GetScalars())
+                frame = frame.reshape(height, width, -1)
+                frame = frame[::-1]
+                frames.append(frame)
+            imageio.mimsave(file_path+'.mp4', frames, fps=30)
---
+++ ui_window.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'brain3d.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QPushButton, QRadioButton, QSizePolicy, QSpinBox, QSlider,
+    QWidget)
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(900, 500)
+        MainWindow.setFixedSize(900, 500)  # 禁止視窗縮放
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+
+        self.BF_lineEdit = QLineEdit(self.centralwidget)
+        self.BF_lineEdit.setObjectName(u"BF_lineEdit")
+        self.BF_lineEdit.setGeometry(QRect(90, 40, 161, 21))
+        self.BF_lineEdit.setClearButtonEnabled(False)
+        self.BF_label = QLabel(self.centralwidget)
+        self.BF_label.setObjectName(u"BF_label")
+        self.BF_label.setGeometry(QRect(20, 40, 60, 21))
+        self.BF_lineEdit.setAcceptDrops(True)
+
+        self.LF_label = QLabel(self.centralwidget)
+        self.LF_label.setObjectName(u"LF_label")
+        self.LF_label.setGeometry(QRect(20, 70, 60, 21))
+        self.LF_lineEdit = QLineEdit(self.centralwidget)
+        self.LF_lineEdit.setObjectName(u"LF_lineEdit")
+        self.LF_lineEdit.setGeometry(QRect(90, 70, 161, 21))
+        self.LF_lineEdit.setEnabled(False)
+        self.LF_lineEdit.setClearButtonEnabled(False)
+        self.LF_lineEdit.setAcceptDrops(True)
+
+        self.PF_label = QLabel(self.centralwidget)
+        self.PF_label.setObjectName(u"PF_label")
+        self.PF_label.setGeometry(QRect(20, 100, 60, 21))
+        self.PF_lineEdit = QLineEdit(self.centralwidget)
+        self.PF_lineEdit.setObjectName(u"PF_lineEdit")
+        self.PF_lineEdit.setGeometry(QRect(90, 100, 161, 21))
+        self.PF_lineEdit.setClearButtonEnabled(False)
+        self.PF_lineEdit.setAcceptDrops(True)
+        self.PF_lineEdit.setEnabled(False)
+
+        self.BF_btn = QPushButton(self.centralwidget)
+        self.BF_btn.setObjectName(u"BF_btn")
+        self.BF_btn.setGeometry(QRect(253, 35, 51, 32))
+        self.LF_btn = QPushButton(self.centralwidget)
+        self.LF_btn.setObjectName(u"LF_btn")
+        self.LF_btn.setGeometry(QRect(253, 65, 51, 32))
+        self.LF_btn.setEnabled(False)
+        self.PF_btn = QPushButton(self.centralwidget)
+        self.PF_btn.setObjectName(u"PF_btn")
+        self.PF_btn.setGeometry(QRect(253, 95, 51, 32))
+        self.PF_btn.setEnabled(False)
+
+        self.BO_label = QLabel(self.centralwidget)
+        self.BO_label.setObjectName(u"BO_label")
+        self.BO_label.setGeometry(QRect(20, 150, 81, 21))
+        self.BO_spinBox = QSpinBox(self.centralwidget)
+        self.BO_spinBox.setObjectName(u"BO_spinBox")
+        self.BO_spinBox.setGeometry(QRect(110, 150, 61, 24))
+        self.BO_spinBox.setMinimum(1)
+        self.BO_spinBox.setMaximum(40)
+        self.BO_spinBox.setValue(20)
+        self.BO_spinBox.setSingleStep(1)
+        self.BO_spinBox.setEnabled(False)
+
+        self.label_label = QLabel(self.centralwidget)
+        self.label_label.setObjectName(u"label_label")
+        self.label_label.setGeometry(QRect(20, 200, 60, 21))
+
+        self.label_op_label = QLabel(self.centralwidget)
+        self.label_op_label.setObjectName(u"label_op_label")
+        self.label_op_label.setGeometry(QRect(20, 230, 91, 21))
+
+        self.LO_spinBox = QSpinBox(self.centralwidget)
+        self.LO_spinBox.setObjectName(u"LO_spinBox")
+        self.LO_spinBox.setGeometry(QRect(110, 230, 61, 24))
+        self.LO_spinBox.setMinimum(1)
+        self.LO_spinBox.setMaximum(40)
+        self.LO_spinBox.setValue(20)
+        self.LO_spinBox.setSingleStep(1)
+        self.LO_spinBox.setEnabled(False)
+
+        self.radioButton_1 = QRadioButton(self.centralwidget)
+        self.radioButton_1.setObjectName(u"radioButton_1")
+        self.radioButton_1.setGeometry(QRect(70, 200, 41, 20))
+        self.radioButton_1.setEnabled(False)
+        self.radioButton_1.setChecked(False)
+        self.radioButton_1.setAutoExclusive(False)
+        self.radioButton_2 = QRadioButton(self.centralwidget)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setGeometry(QRect(110, 200, 41, 20))
+        self.radioButton_2.setEnabled(False)
+        self.radioButton_2.setAutoExclusive(False)
+        self.radioButton_3 = QRadioButton(self.centralwidget)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.radioButton_3.setGeometry(QRect(150, 200, 41, 20))
+        self.radioButton_3.setEnabled(False)
+        self.radioButton_3.setAutoExclusive(False)
+        self.radioButton_4 = QRadioButton(self.centralwidget)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setGeometry(QRect(190, 200, 41, 20))
+        self.radioButton_4.setEnabled(False)
+        self.radioButton_4.setAutoExclusive(False)
+        self.radioButton_5 = QRadioButton(self.centralwidget)
+        self.radioButton_5.setObjectName(u"radioButton_5")
+        self.radioButton_5.setGeometry(QRect(230, 200, 41, 20))
+        self.radioButton_5.setEnabled(False)
+        self.radioButton_5.setAutoExclusive(False)
+
+        self.pred_label = QLabel(self.centralwidget)
+        self.pred_label.setObjectName(u"pred_label")
+        self.pred_label.setGeometry(QRect(20, 280, 60, 21))
+
+        self.radioButton_tp = QRadioButton(self.centralwidget)
+        self.radioButton_tp.setObjectName(u"radioButton_tp")
+        self.radioButton_tp.setGeometry(QRect(70, 280, 41, 20))
+        self.radioButton_tp.setEnabled(False)
+        self.radioButton_tp.setAutoExclusive(False)
+        self.radioButton_fp = QRadioButton(self.centralwidget)
+        self.radioButton_fp.setObjectName(u"radioButton_fp")
+        self.radioButton_fp.setGeometry(QRect(120, 280, 41, 20))
+        self.radioButton_fp.setEnabled(False)
+        self.radioButton_fp.setAutoExclusive(False)
+        self.radioButton_fn = QRadioButton(self.centralwidget)
+        self.radioButton_fn.setObjectName(u"radioButton_fn")
+        self.radioButton_fn.setGeometry(QRect(170, 280, 41, 20))
+        self.radioButton_fn.setEnabled(False)
+        self.radioButton_fn.setAutoExclusive(False)
+
+        self.pred_op_label = QLabel(self.centralwidget)
+        self.pred_op_label.setObjectName(u"pred_op_label")
+        self.pred_op_label.setGeometry(QRect(20, 310, 91, 21))
+
+        self.PO_spinBox = QSpinBox(self.centralwidget)
+        self.PO_spinBox.setObjectName(u"PO_spinBox")
+        self.PO_spinBox.setGeometry(QRect(110, 310, 61, 24))
+        self.PO_spinBox.setMinimum(1)
+        self.PO_spinBox.setMaximum(40)
+        self.PO_spinBox.setValue(20)
+        self.PO_spinBox.setSingleStep(1)
+        self.PO_spinBox.setEnabled(False)
+
+        self.render = QWidget(self.centralwidget)
+        self.render.setObjectName(u"render")
+        self.render.setGeometry(QRect(320, 20, 561, 461))
+
+        self.render_pushButton = QPushButton(self.centralwidget)
+        self.render_pushButton.setObjectName(u"render_pushButton")
+        self.render_pushButton.setGeometry(QRect(30, 360, 113, 32))
+        self.render_pushButton.setEnabled(False)
+
+        self.save_pushButton = QPushButton(self.centralwidget)
+        self.save_pushButton.setObjectName(u"save_pushButton")
+        self.save_pushButton.setGeometry(QRect(160, 360, 113, 32))
+        self.save_pushButton.setEnabled(False)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.BF_label.setText(QCoreApplication.translate("MainWindow", u"Brain File:", None))
+        self.LF_label.setText(QCoreApplication.translate("MainWindow", u"Label File:", None))
+        self.PF_label.setText(QCoreApplication.translate("MainWindow", u"Pred File:", None))
+        self.BO_label.setText(QCoreApplication.translate("MainWindow", u"Brain Opacity", None))
+        self.label_label.setText(QCoreApplication.translate("MainWindow", u"Label", None))
+        self.label_op_label.setText(QCoreApplication.translate("MainWindow", u"Label Opacity", None))
+        self.radioButton_1.setText(QCoreApplication.translate("MainWindow", u"1", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"2", None))
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"3", None))
+        self.radioButton_4.setText(QCoreApplication.translate("MainWindow", u"4", None))
+        self.radioButton_5.setText(QCoreApplication.translate("MainWindow", u"5", None))
+        self.render_pushButton.setText(QCoreApplication.translate("MainWindow", u"Render", None))
+        self.save_pushButton.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.pred_label.setText(QCoreApplication.translate("MainWindow", u"Pred", None))
+        self.pred_op_label.setText(QCoreApplication.translate("MainWindow", u"Pred Opacity", None))
+        self.radioButton_tp.setText(QCoreApplication.translate("MainWindow", u"TP", None))
+        self.radioButton_fp.setText(QCoreApplication.translate("MainWindow", u"FP", None))
+        self.radioButton_fn.setText(QCoreApplication.translate("MainWindow", u"FN", None))
+        self.BF_btn.setText(QCoreApplication.translate("MainWindow", u"^", None))
+        self.LF_btn.setText(QCoreApplication.translate("MainWindow", u"^", None))
+        self.PF_btn.setText(QCoreApplication.translate("MainWindow", u"^", None))
+    # retranslateUi
+
