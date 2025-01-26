e8c31c41ec547fa22b57b6e897b275cc9909d39f
rightpunchChen <rightpunch1123@gmail.com>
2025-01-26 15:31:52 +0800

    Delete utiles directory

--- utiles/configs.py
+++
@@ -1,9 +0,0 @@
-BACKGROUND_COLORS = (0.53, 0.81, 0.92) # Background color
-BRAIN_COLORS = (1.0,0.9,0.9)            # Brain color
-MASK_COLORS = [(1, 1, 0),
-                (1, 0, 0),
-                (0, 1, 0)]              # Label color
-TP_COLOR = (0, 0, 1)                    # True positive color
-FP_COLOR = (1, 0, 1)                    # False positive color
-FN_COLOR = (0, 1, 1)                    # False negative color
-LABEL_NUM = 5
