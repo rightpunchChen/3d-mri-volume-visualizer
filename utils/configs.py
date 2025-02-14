5c51fb661e58d11289d86c39c00835ca661b583f
Charles Chen <rightpunch1123@gmail.com>
2025-02-14 11:04:18 +0800

    Update configs.py

--- utils/configs.py
+++ utils/configs.py
@@ -1,9 +1,11 @@
-BACKGROUND_COLORS = (0.53, 0.81, 0.92) # Background color
+BACKGROUND_COLORS = (0.82, 0.82, 0.82) # Background color
 BRAIN_COLORS = (1.0,0.9,0.9)            # Brain color
-MASK_COLORS = [(1, 1, 0),
-                (1, 0, 0),
-                (0, 1, 0)]              # Label color
-TP_COLOR = (0, 0, 1)                    # True positive color
-FP_COLOR = (1, 0, 1)                    # False positive color
-FN_COLOR = (0, 1, 1)                    # False negative color
+MASK_COLORS = [(1, 0, 0),
+                (1, 1, 0),
+                (0, 1, 0),
+                (0, 0, 1),
+                (0, 1, 1)]              # Label color
+TP_COLOR = (0.36, 0.68, 0.68)                    # True positive color
+FP_COLOR = (0.56, 0.1, 1)                    # False positive color
+FN_COLOR = (1, 0.5, 0)                    # False negative color
 LABEL_NUM = 5
